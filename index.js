c1e410c013cc7a56d8d447adf160cad631d81aa2
til2to <padwusup@gmail.com>
2022-12-21 09:39:26 +0000

    Menu overlay to dispear on the click of any menu item

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.menuImg');
 const menuList = document.querySelector('.menu_items');
 const closeHamburger = document.querySelector('#btn_close');
 const logoName = document.querySelector('.logo-name');
+const lists = document.querySelectorAll('.menu_items li a');
 
 hamburger.addEventListener('click', () => {
   if (menuList.style.display !== 'block') {
@@ -15,4 +16,12 @@ closeHamburger.addEventListener('click', () => {
   menuList.style.display = 'none';
   logoName.style.display = 'block';
   hamburger.style.display = 'block';
+});
+
+lists.forEach((list) => {
+  list.addEventListener('click', () => {
+    menuList.style.display = 'none';
+    logoName.style.display = 'block';
+    hamburger.style.display = 'block';
+  });
 });
