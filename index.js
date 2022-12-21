e108c1ac5a657be894fd56d9451bd763beecef7a
til2to <padwusup@gmail.com>
2022-12-21 08:20:10 +0000

    Create mobile menu popup

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const hamburger = document.querySelector('.menuImg');
 const menuList = document.querySelector('.menu_items');
 const closeHamburger = document.querySelector('#btn_close');
 const logoName = document.querySelector('.logo-name');
+const projectContainer = document.querySelector('#seeProjects')
+const modalMainContainer = document.querySelector('#popup-container')
+const modalContainer = document.querySelector('#modal');
+const blurGround = document.querySelector('#blurBg');
+
 
 hamburger.addEventListener('click', () => {
   if (menuList.style.display !== 'block') {
@@ -15,4 +20,141 @@ closeHamburger.addEventListener('click', () => {
   menuList.style.display = 'none';
   logoName.style.display = 'block';
   hamburger.style.display = 'block';
+});
+
+const projectArray = [
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    img: './images/placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkDemo: 'https://til2to.github.io/porfolio_projects/',
+    sourceCode: 'https://github.com/til2to/porfolio_projects/tree/main',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    img: './images/placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkDemo: 'https://til2to.github.io/porfolio_projects/',
+    sourceCode: 'https://github.com/til2to/porfolio_projects/tree/main',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    img: './images/placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkDemo: 'https://til2to.github.io/porfolio_projects/',
+    sourceCode: 'https://github.com/til2to/porfolio_projects/tree/main',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    img: './images/placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkDemo: 'https://til2to.github.io/porfolio_projects/',
+    sourceCode: 'https://github.com/til2to/porfolio_projects/tree/main',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    img: './images/placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkDemo: 'https://til2to.github.io/porfolio_projects/',
+    sourceCode: 'https://github.com/til2to/porfolio_projects/tree/main',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    img: './images/placeholder.png',
+    technologies: ['html', 'bootstrap', 'Ruby'],
+    linkDemo: 'https://til2to.github.io/porfolio_projects/',
+    sourceCode: 'https://github.com/til2to/porfolio_projects/tree/main',
+  },
+];
+
+projectArray.forEach((project, index) => {
+  let tech = '';
+  project.technologies.forEach((item) => {
+    tech += `<li class="tech-item">
+    <span>${item}</span>
+    </li>`;
+  });
+
+  const projectItem = `
+    <div id="cardWork">
+      <div class="prjectDescription">
+        <h3>
+          ${project.name}
+        </h3>
+        <p>
+          ${project.description}
+        </p>
+      </div>
+      <ul>
+        ${tech}
+      </ul>
+      <button class="recent-work-project-btn" id=${index}>See Project</button>
+    </div>`;
+
+  projectContainer.innerHTML += projectItem;
+});
+
+const projectBtns = document.querySelectorAll('.recent-work-project-btn');
+const modalBtn = document.querySelector('#modal-close');
+
+modalBtn.addEventListener('click', () => {
+  modalMainContainer.style.display = 'none';
+  blurGround.style.filter = 'blur(0)';
+});
+
+const modalContent = (project) => {
+  blurGround.style.filter = 'blur(3px)';
+
+  const modal = `
+  <div>
+    <h2>${project.name}</h2>
+  </div>
+
+  <ul class="stack-tags">
+    <li>html</li>
+    <li>Bootstrap</li>
+    <li>Ruby on rails</li>
+  </ul>
+
+  <div class="modal-image">
+    <img src='./images/SnapshootPortfolio.svg' alt="Project Image">
+  </div>
+  <p>
+    Lorem Ipsum is simply dummy text of the printing and typesetting 
+    industry. Lorem Ipsum has been the industry's standard dummy text 
+    ever since the 1500s, when an unknown printer took a galley of type 
+    and scrambled it to make a type specimen book. It has survived not 
+    only five centuries, but also the leap into electronic typesetting, 
+    remaining essent
+  </p>
+
+  <section id="button-group">
+    <button type="button" class="button-group-items demo-button">
+      <span>See Live</span> 
+      <span><img class="live-icon" src="./images/live.svg" alt="live icon"></span>
+    </button>
+    <button type="button" class="button-group-items source-button">
+      <span>See Source</span> 
+      <span><img class="github-icon" src="./images/one.png" alt="live icon"></span>
+    </button> 
+  </section>
+  `;
+
+  modalContainer.innerHTML = modal;
+  modalMainContainer.style.display = 'block';
+};
+
+projectBtns.forEach((projectBtn) => {
+  projectBtn.addEventListener('click', (e) => {
+    console.log('see project')
+    const getBtnId = e.target.getAttribute('id');
+    const projectObj = projectArray[getBtnId];
+    modalContent(projectObj);
+  });
 });
